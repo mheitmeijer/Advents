83a9f00684c6e753b2d2cb956a99a85383f9900f
Heitmeijer, Marcel <marcel.heitmeijer@sogeti.com>
2020-12-02 22:20:08 +0100

    Advent 03

---
+++ Advent03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Advent03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+
+        private static void Part1()
+        {
+            Console.ReadLine();
+        }
+
+        private static void Part2()
+        {
+            Console.ReadLine();
+        }
+    }
+}
